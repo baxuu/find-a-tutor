da0787f84c6221da996d3663c2697aee366b9c1c
unknown <krolczyk.artur@gmail.com>
2021-03-03 20:15:22 +0100

    added routes

--- router.js
+++ router.js
@@ -1,1 +1,39 @@
-import { createRouter } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/tutors',
+    },
+    {
+      path: '/tutors',
+      component: null,
+    },
+    {
+      path: '/tutors/:id',
+      component: null,
+      children: [
+        {
+          path: '/contact',
+          component: null,
+        },
+      ],
+    },
+    {
+      path: '/signup',
+      component: null,
+    },
+    {
+      path: '/messages',
+      component: null,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: null,
+    },
+  ],
+});
+
+export default router;
